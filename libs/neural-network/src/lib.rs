9a83b8a074d5621661bfc89309952cdf1835112d
Michael Caira <mbcaira@gmail.com>
2022-09-22 22:24:32 -0400

    begin neural network setup

---
+++ libs/neural-network/src/lib.rs
@@ -0,0 +1,41 @@
+pub struct Network {
+    layers: Vec<Layer>,
+}
+
+struct Layer {
+    neurons: Vec<Neuron>,
+}
+
+struct Neuron {
+    bias: f32,
+    weights: Vec<f32>,
+}
+
+impl Network {
+    pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.layers
+            .iter()
+            .fold(inputs, |inputs, layer| layer.propagate(inputs))
+    }
+}
+
+impl Layer {
+    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
+        self.neurons
+            .iter()
+            .map(|neuron| neuron.propagate(&inputs))
+            .collect()
+    }
+}
+
+impl Neuron {
+    fn propagate(&self, inputs: &[f32]) -> f32 {
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+    }
+}
