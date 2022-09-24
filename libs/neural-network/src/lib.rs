de0c1b7b5622b8ca181d10b28b758a315979b404
Michael Caira <mbcaira@gmail.com>
2022-09-24 01:23:46 -0400

    restructure files

--- libs/neural-network/src/lib.rs
+++ libs/neural-network/src/lib.rs
@@ -1,17 +1,32 @@
+pub use self::layer_topology::*;
+
+use self::{layer::*, neuron::*};
+use rand::{Rng, RngCore};
+
+mod layer;
+mod layer_topology;
+mod neuron;
+
+#[derive(Clone, Debug)]
 pub struct Network {
     layers: Vec<Layer>,
 }
 
-struct Layer {
-    neurons: Vec<Neuron>,
-}
-
-struct Neuron {
-    bias: f32,
-    weights: Vec<f32>,
-}
-
 impl Network {
+    pub(crate) fn new(layers: Vec<Layer>) -> Self {
+        Self { layers }
+    }
+
+    pub fn random(rng: &mut dyn rand::RngCore, layers: &[LayerTopology]) -> Self {
+        assert!(layers.len() > 1);
+        let layers = layers
+            .windows(2)
+            .map(|layers| Layer::random(rng, layers[0].neurons, layers[1].neurons))
+            .collect();
+
+        Self::new(layers)
+    }
+
     pub fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
         self.layers
             .iter()
@@ -19,23 +34,71 @@ impl Network {
     }
 }
 
-impl Layer {
-    fn propagate(&self, inputs: Vec<f32>) -> Vec<f32> {
-        self.neurons
-            .iter()
-            .map(|neuron| neuron.propagate(&inputs))
-            .collect()
+#[cfg(test)]
+mod tests {
+    use super::*;
+    mod random {
+        use super::*;
+
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+
+            let network = Network::random(
+                &mut rng,
+                &[
+                    LayerTopology { neurons: 3 },
+                    LayerTopology { neurons: 2 },
+                    LayerTopology { neurons: 1 },
+                ],
+            );
+
+            assert_eq!(network.layers.len(), 2);
+            assert_eq!(network.layers[0].neurons.len(), 2);
+
+            approx::assert_relative_eq!(network.layers[0].neurons[0].bias, -0.6255188);
+
+            approx::assert_relative_eq!(
+                network.layers[0].neurons[0].weights.as_slice(),
+                &[0.67383957, 0.8181262, 0.26284897].as_slice()
+            );
+
+            approx::assert_relative_eq!(network.layers[0].neurons[1].bias, 0.5238807);
+
+            approx::assert_relative_eq!(
+                network.layers[0].neurons[1].weights.as_slice(),
+                &[-0.5351684, 0.069369555, -0.7648182].as_slice()
+            );
+
+            assert_eq!(network.layers[1].neurons.len(), 1);
+
+            approx::assert_relative_eq!(
+                network.layers[1].neurons[0].weights.as_slice(),
+                &[-0.48879623, -0.19277143].as_slice()
+            );
+        }
     }
-}
 
-impl Neuron {
-    fn propagate(&self, inputs: &[f32]) -> f32 {
-        let output = inputs
-            .iter()
-            .zip(&self.weights)
-            .map(|(input, weight)| input * weight)
-            .sum::<f32>();
+    mod propagate {
+        use super::*;
 
-        (self.bias + output).max(0.0)
+        #[test]
+        fn test() {
+            let layers = (
+                Layer::new(vec![
+                    Neuron::new(0.0, vec![-0.5, -0.4, -0.3]),
+                    Neuron::new(0.0, vec![-0.2, -0.1, 0.0]),
+                ]),
+                Layer::new(vec![Neuron::new(0.0, vec![-0.5, 0.5])]),
+            );
+            let network = Network::new(vec![layers.0.clone(), layers.1.clone()]);
+
+            let actual = network.propagate(vec![0.5, 0.6, 0.7]);
+            let expected = layers.1.propagate(layers.0.propagate(vec![0.5, 0.6, 0.7]));
+
+            approx::assert_relative_eq!(actual.as_slice(), expected.as_slice());
+        }
     }
 }
---
+++ libs/neural-network/src/neuron.rs
@@ -0,0 +1,83 @@
+use crate::*;
+
+#[derive(Clone, Debug)]
+pub struct Neuron {
+    pub(crate) bias: f32,
+    pub(crate) weights: Vec<f32>,
+}
+
+impl Neuron {
+    pub fn new(bias: f32, weights: Vec<f32>) -> Self {
+        assert!(!weights.is_empty());
+        Neuron { bias, weights }
+    }
+
+    pub fn random(rng: &mut dyn rand::RngCore, output_size: usize) -> Self {
+        let bias = rng.gen_range(-1.0..=1.0);
+        let weights = (0..output_size)
+            .map(|_| rng.gen_range(-1.0..=1.0))
+            .collect();
+
+        Self::new(bias, weights)
+    }
+
+    pub fn propagate(&self, inputs: &[f32]) -> f32 {
+        let output = inputs
+            .iter()
+            .zip(&self.weights)
+            .map(|(input, weight)| input * weight)
+            .sum::<f32>();
+
+        (self.bias + output).max(0.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod random {
+        use super::*;
+        use rand::SeedableRng;
+        use rand_chacha::ChaCha8Rng;
+
+        #[test]
+        fn test() {
+            let mut rng = ChaCha8Rng::from_seed(Default::default());
+            let neuron = Neuron::random(&mut rng, 4);
+
+            approx::assert_relative_eq!(neuron.bias, -0.6255188);
+            approx::assert_relative_eq!(
+                neuron.weights.as_slice(),
+                [0.67383957, 0.8181262, 0.26284897, 0.5238807].as_ref()
+            );
+        }
+    }
+
+    mod propagate {
+        use super::*;
+
+        #[test]
+        fn returns_propagated_input() {
+            let actual = Neuron::new(0.1, vec![-0.3, 0.6, 0.9]).propagate(&[0.5, -0.6, 0.7]);
+            let expected: f32 = 0.1 + (0.5 * -0.3) + (-0.6 * 0.6) + (0.7 * 0.9);
+
+            approx::assert_relative_eq!(actual, expected.max(0.0));
+        }
+
+        #[test]
+        fn restricts_output() {
+            let neuron = Neuron::new(0.0, vec![0.5]);
+            let v1 = neuron.propagate(&[-1.0]);
+            let v2 = neuron.propagate(&[-0.5]);
+            let v3 = neuron.propagate(&[0.0]);
+            let v4 = neuron.propagate(&[0.5]);
+            let v5 = neuron.propagate(&[1.0]);
+
+            approx::assert_relative_eq!(v1, v2);
+            approx::assert_relative_eq!(v2, v3);
+            approx::assert_relative_ne!(v3, v4);
+            approx::assert_relative_ne!(v4, v5);
+        }
+    }
+}
